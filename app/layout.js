7c03bebf1264d91b238ececd1f137deb51f6c95f
Max <max@mshrentals.com>
2026-03-29 15:09:15 -0400

    fix: add root layout

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Toll Recovery Test',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
